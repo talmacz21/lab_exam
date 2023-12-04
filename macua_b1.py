a92e158365cc6cdb9ff1ba1d3fbfd02cc21ae9a3
talmacz21 <149986321+talmacz21@users.noreply.github.com>
2023-12-04 11:46:44 +0800

    Add files via upload

--- macua_b1.py
+++ macua_b1.py
@@ -1,9 +1,24 @@
-# Load the CSV data into a DataFrame
-from pkg_resources import ExtractionError
-with open("./Exam_Table.csv", 'r') as file:
-        csvreader = csv.reader(file)
-        for row in csvreader:
-         print(row)
-#extract rows
-ExtractionError ;row (30,0)
-print(row)
+import pandas as pd 
+import pandas as pd
+import csv
+
+#Column titles in your .CSV file
+col_list = ["Interval"]
+
+#Read from file and seprate a specific column
+data = pd.read_csv("words.csv", usecols=col_list)["Interval"]
+#Change extracted data to a list
+ls = list(data)
+for n, item in enumerate(ls):
+  #For example capitalize all words in list
+	ls[n] = [str(item)range(30-0)]
+
+#Show all changed data
+for n, item in enumerate(ls):
+  print(f"{n} : {item}")
+
+  #Write changes to a new file
+with open('b1_output.csv', "w") as s:
+    w = csv.writer(s)
+    for word in ls:
+        w.writerow(word)
